98ecf35af6bcfc04cf31875eb357ad75bec350e6
Connor Yager <csy7ay@virginia.edu>
2021-10-24 21:31:23 -0400

    initial commit

---
+++ get_exercises/app.py
@@ -0,0 +1,22 @@
+import json
+import os
+import boto3
+
+# import requests
+
+table_name = os.environ['TABLE_NAME']
+dynamodb = boto3.client("dynamodb")
+
+def lambda_handler(event, context):
+
+    print(table_name)
+    entries = dynamodb.scan(
+        TableName=table_name
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": entries
+        })
+    }
