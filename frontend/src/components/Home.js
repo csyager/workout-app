a9b18f4aadac0d498b98fa7e9b6f17a921f7692f
Connor Yager <csy7ay@virginia.edu>
2021-11-03 22:51:52 -0400

    added font-awesome icons to home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,21 +5,23 @@ import Col from 'react-bootstrap/Col'
 import {
     Link
 } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDay, faDumbbell, faRunning} from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
     return (
         <Container>
             <Row>
                 {/* eslint-disable-next-line */}
-                <Button variant="primary" as={Col, Link} to="/calendar" className="home-button">Calendar</Button>
+                <Button variant="primary" as={Col, Link} to="/calendar" className="home-button"><FontAwesomeIcon icon={faCalendarDay} /> Calendar</Button>
             </Row>
             <Row>
                 {/* eslint-disable-next-line */}
-                <Button variant="secondary" as={Col, Link} to="/exercises" className="home-button">Exercises</Button>
+                <Button variant="secondary" as={Col, Link} to="/exercises" className="home-button"><FontAwesomeIcon icon={faDumbbell} /> Exercises</Button>
             </Row>
             <Row>
                 {/* eslint-disable-next-line */}
-                <Button variant="success" as={Col, Link} to="/workout" className="home-button">Workout</Button>
+                <Button variant="success" as={Col, Link} to="/workout" className="home-button"><FontAwesomeIcon icon={faRunning} /> Workout</Button>
             </Row>
         </Container>
     )
